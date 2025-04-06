0788c7f7e45f62405f5eb1bb0fb01da3479953a9
derinemesis <theboncud34@yahoo.com>
2025-04-06 13:02:49 +0700

    Create allscript.js

---
+++ allscript.js
@@ -0,0 +1,9 @@
+// Mengimpor skrip dari folder trojan
+import './trojan/trojan.js';
+import './trojan/TrojanWsProxypr.js';
+
+// Mengimpor skrip dari folder vmess
+import './vmess/vmess.js';
+
+// Mengimpor skrip dari folder vless
+import './vless/vless.js';
